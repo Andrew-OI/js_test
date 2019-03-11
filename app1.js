175fa951af28b22560ea20c7ec9a4c1a383eb1c7
andriyoprofat <andriy.oprofat@eleks.com>
2019-03-11 16:31:00 +0200

    Hometask after lesson 2.

---
+++ app1.js
@@ -0,0 +1,27 @@
+let x = 10;
+let y = 15;
+console.log(x + y); // computing and printing the sum of two integers.
+
+let x = "work";
+let y = "Test_";
+console.log(y + x); // creating a new string adding “Test_" in front of a given string.
+
+let name = "Andrew";
+let lastname = "Oprofat";
+let age = 31;
+console.log(name + " " + lastname + " " + 31); //combining and printintong my first, last name and age.
+
+let x = 25;
+console.log(x < 100); // checking a given integer is bigger then 100.
+
+let x = 150;
+let y = 100;
+let z = 200;
+console.log(x > (y + z)); // finding if the first number is larger from the two given positive integers.
+
+let x = 50;
+let y = 200;
+console.log(x == 50 || y == 50 || x + y == 50); // checking two given numbers and return true if one of the number is 50 or if their sum is 50.
+
+let x = 5;
+console.log(x % 3 == 0 || x % 7 == 0); // checking if a given positive number is a multiple of 3 or a multiple of 7.
