7c131998fa7670efaaa4a4410cb77eb8c1446651
Andrew-OI <48097609+Andrew-OI@users.noreply.github.com>
2019-04-18 17:21:26 +0300

    delete2

--- Hometask8-3.js
+++
@@ -1,9 +0,0 @@
-// Make some change to object and append is as new line to existed file
-
-const fs = require("fs");
-
-fs.appendFile("/Git/js_test/student_file.txt","New line added", function (err) {
-     if (err) throw err;
- console.log('Done!');
-
-});
